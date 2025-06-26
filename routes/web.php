f9a4cd62ee4c5da2b464698521f5c09c1fa076be
nam246 <nam24620@gmail.com>
2025-06-26 16:16:48 +0700

    Fix 'volume is banned in railway'

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome', []);
 });
-
-Route::get('/articles', function () {
-    return view('articles', []);
-});
